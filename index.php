b1d2d78d0b5aaa96d49169ddae9900710cf9c17b
Henrell <henrellmusic@gmail.com>
2015-12-08 15:40:14 -0200

    Index
    Home do site.

---
+++ index.php
@@ -0,0 +1,108 @@
+<html>
+<head>
+	<title>LUIZ COSTA DEVELOPER</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
+	<link rel="stylesheet" type="text/css" href="css/main.css" rel="stylesheet"/>
+	<link rel="stylesheet" href="css/bootstrap.css" media="screen" rel="stylesheet"/>
+	<link rel="stylesheet" href="css/flexslider.css" media="screen" rel="stylesheet"/>
+	<link href='https://fonts.googleapis.com/css?family=Allerta|Lato:400,300|Oswald:400,300' rel='stylesheet' type='text/css'>
+	<script src="js/jquery.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/flexslider.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
+	<meta charset="utf-8"/>
+</head>
+<body>
+<!--	Header -->
+<?php require_once ('header.php'); ?>
+
+<section class="main projects" id="projects">
+	<div class="container">
+		<h2><span>Trampos / Freelas</span></h2>
+		<article class="col-md-6">
+			<a href="javascript:" title="" class="box col-md-12" rel="pinheiro">
+				<span class="capa">
+					<img src="prints/pinheironeto.jpg" alt=""/>
+				</span>
+			</a>
+		</article>
+		<article class="col-md-6">
+			<a href="javascript:" title="" class="box col-md-12" rel="pga">
+				<span class="capa">
+					<img src="prints/pgadvogados.jpg" alt=""/>
+				</span>
+			</a>
+		</article>
+		<article class="col-md-6">
+			<a href="javascript:" title="" class="box col-md-12" rel="bma">
+				<span class="capa">
+					<img src="prints/bmalaw.jpg" alt=""/>
+				</span>
+			</a>
+		</article>
+		<article class="col-md-6">
+			<a href="javascript:" title="" class="box col-md-12" rel="mattosfilho">
+				<span class="capa">
+					<img src="prints/mattosfilho.jpg" alt=""/>
+				</span>
+			</a>
+		</article>
+	</div>
+</section>
+<section  class="main me" id="me">
+	<div class="container">
+		<h2><span>Quem sou eu</span></h2>
+		<p>Seja bem-vindo ao meu portfólio. Meu nome é Luiz Henrique Costa, mas pode me chamar de Rick. Atuo na área de desenvolvimento web
+			a mais ou menos 4 anos. Nos últimos 3 anos tenho me dedicado à desenvolvimento para SharePoint, um tipo de CMS desenvolvido pela microsoft para atender empresas de médio / grande porte. Aqui você poderá conhecer um pouco do que eu faço, meus trabalhos, freelas, desenvolvimentos e tecnologias que estou acostumado a utilizar.
+		</p>
+		
+		<div class="col-md-12 col-lg-6">
+			<h3><span>Skills</span></h3>
+
+			<ul class="list-custom skills">
+				<li><span class="percent 90"></span><span class="text">XHTML / HTML 5</span></li>
+				<li><span class="percent 90"></span><span class="text">CSS 3</span></li>
+				<li><span class="percent 70"></span><span class="text">jQuery</span></li>
+				<li><span class="percent 50"></span><span class="text">JavaScript</span></li>
+				<li><span class="percent 50"></span><span class="text">Bootstrap</span></li>
+				<li><span class="percent 80"></span><span class="text">SharePoint</span></li>
+				<li><span class="percent 80"></span><span class="text">XSLT</span></li>
+				<li><span class="percent 80"></span><span class="text">Masterpages</span></li>
+				<li><span class="percent 80"></span><span class="text">PageLayouts</span></li>
+				<li><span class="percent 80"></span><span class="text">Custom Data Views</span></li>
+				<li><span class="percent 60"></span><span class="text">Wordpress</span></li>
+				<li><span class="percent 50"></span><span class="text">PHP / MySQL</span></li>
+			</ul>	
+		</div>
+
+		<div class="col-md-12 col-lg-6">
+			<h3><span>Softwares</span></h3>
+
+			<ul class="list-custom skills">
+				<li><span class="percent 60"></span><span class="text">Sharepoint Designer</span></li>
+				<li><span class="percent 50"></span><span class="text">Sublime Text</span></li>
+				<li><span class="percent 60"></span><span class="text">Photoshop</span></li>
+				<li><span class="percent 50"></span><span class="text">FireWorks</span></li>
+				<li><span class="percent 50"></span><span class="text">Chrome / Mozilla / Safari / IE</span></li>
+			</ul>	
+		</div>		
+	</div>
+</section>
+<section class="main contact" id="contact">
+	<div class="container">
+		<h2><span>Entre em Contato</span></h2>
+		<div class="">
+			<p>Para entrar em contato comigo, fazer um orçamento ou tirar dúvidas, mande um e-mail para:</p>
+			<p>
+				<a href="mailto:contato@luizcostadeveloper.com.br" title="Enviar E-mail">contato@luizcostadeveloper.com.br</a> / 
+				<a href="https://br.linkedin.com/in/luizhcosta" target="_blank">linkedin</a>
+			</p>
+		</div>
+	</div>
+</section>
+
+<!--	Footer 	-->
+<?php require_once('footer.php') ?>
+</body>
+</html>
